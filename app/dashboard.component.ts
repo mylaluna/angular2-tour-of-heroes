db9faecb5fae129d847425e12f828c0aa7d8e8fb
Rain Zhang <rzhang@objectmastery.com>
2016-03-29 17:18:12 +1100

    Learning Angular 2 - Routing 2

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,6 +1,16 @@
 import { Component } from 'angular2/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 @Component({
 	selector: 'my-dashboard',
-	template: '<h3>My Dashboard</h3>'
+	templateUrl: 'app/dashboard.component.html'
 })
-export class DashboardComponent { }
+export class DashboardComponent implements OnInit {
+	heroes: Hero[] = [];
+	constructor(private _heroService: HeroService) { }
+	ngOnInit() {
+		this._heroService.getHeroes()
+			.then(heroes => this.heroes = heroes.slice(1, 5));
+	}
+	gotoDetail() { /* not implemented yet */ }
+}
