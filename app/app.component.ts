669a92f694cfeb65692ae4dd8af4b177fe76fbf3
Rain Zhang <rzhang@objectmastery.com>
2016-04-01 17:45:22 +1100

    Learning Angular 2 - Routing (Continue)

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from 'angular2/core';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
 import { HeroService }     from './hero.service';
 import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
 import { DashboardComponent } from './dashboard.component';
 @Component({
   selector: 'my-app',
@@ -30,6 +31,11 @@ import { DashboardComponent } from './dashboard.component';
     path: '/heroes',
     name: 'Heroes',
     component: HeroesComponent
+  },
+  {
+    path: '/detail/:id',
+    name: 'HeroDetail',
+    component: HeroDetailComponent
   }
 ])
 export class AppComponent {
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -3,6 +3,12 @@ import {Hero} from './hero';
 import {HEROES} from './mock-heroes';
 @Injectable()
 export class HeroService {
+	getHero(id: number) {
+		return Promise.resolve(HEROES).then(
+		  heroes => heroes.filter(hero => hero.id === id)[0]
+		);
+	}
+
 	getHeroes() {
 		return Promise.resolve(HEROES);
 	}
